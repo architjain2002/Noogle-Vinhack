1330059faa1c73792fba6231ca33494fb19249ff
Archit Jain <arcyjain2002@gmail.com>
2023-01-14 03:53:02 +0530

    some minor fixes

--- doccy/extension.js
+++ doccy/extension.js
@@ -20,102 +20,105 @@ function activate(context) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand("getFileText", function () {
-    // The code you place here will be executed every time your command is executed
-    const editor = vscode.window.activeTextEditor;
+  let disposable = vscode.commands.registerCommand(
+    "doccy.getText",
+    function () {
+      // The code you place here will be executed every time your command is executed
+      const editor = vscode.window.activeTextEditor;
 
-    // let document, filename;
-    if (editor) {
-      let document = editor.document;
-      let filename = path.basename(document.fileName);
-      // console.log(filename);
-      const documentContent = document.getText();
-      // console.log(documentContent);
-      for (var i = filename.length; i > 0; i--) {
-        if (filename[i] === ".") {
-          filename = filename.slice(i + 1, filename.length);
-          break;
-        }
-      }
-      vscode.window.showInformationMessage(filename);
-
-      if (filename === "cpp") {
-        let reg = /#include\s*[<"]([^>"]+)[>"]/g;
-        let match;
-        while ((match = reg.exec(documentContent)) !== null) {
-          console.log(match[1]);
-        }
-      } else if (filename == "py") {
-        let reg = /(?:import|from)\s([\w.]+)/g;
-        let match;
-        while ((match = reg.exec(documentContent)) !== null) {
-          console.log(match[1]);
-        }
-      } else if (filename == "java") {
-        let reg = /(?:import)\s([\w.]+)/g;
-        let match;
-        while ((match = reg.exec(documentContent)) !== null) {
-          console.log(match[1]);
-        }
-      }
-    }
-
-    // @ts-ignore
-    fetch(
-      "https://www.googleapis.com/customsearch/v1?key=AIzaSyC6gKMMoXgPkOnnC1TTj6C60aeLxI1v_ys&cx=32c6f4cdb22f343b0&q=tensorflow"
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        vscode.window.showInformationMessage(
-          `Data received: ${JSON.stringify(data)}`
-        );
-        console.log(JSON.stringify(data, null, 2));
-      })
-      .catch((error) => {
-        vscode.window.showErrorMessage(`Error: ${error}`);
-      });
-
-    let panel = vscode.window.createWebviewPanel(
-      "browser", // Identifies the type of the webview. Used internally
-      "Browser", // Title of the panel displayed to the user
-      vscode.ViewColumn.One, // Editor column to show the new webview panel in.
-      {
-        enableScripts: true,
-      }
-    );
-
-    // Navigate to a website
-    panel.webview.html = `<html><body>Loading...</body></html>`;
-
-    // Use XMLHttpRequest to fetch the HTML content of the website
-    var XMLHttpRequest = require("xhr2");
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://www.tensorflow.org", true);
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        // Update the webview's HTML content
-        panel.webview.html = xhr.responseText;
-      }
-    };
-    xhr.send();
-
-    // Handle navigation events
-    panel.webview.onDidReceiveMessage(
-      (message) => {
-        switch (message.command) {
-          case "navigate":
-            xhr.open("GET", message.url, true);
-            xhr.send();
+      // let document, filename;
+      if (editor) {
+        let document = editor.document;
+        let filename = path.basename(document.fileName);
+        // console.log(filename);
+        const documentContent = document.getText();
+        // console.log(documentContent);
+        for (var i = filename.length; i > 0; i--) {
+          if (filename[i] === ".") {
+            filename = filename.slice(i + 1, filename.length);
             break;
+          }
         }
-      },
-      null,
-      context.subscriptions
-    );
+        vscode.window.showInformationMessage(filename);
 
-    // Display a message box to the user
-    // vscode.window.showInformationMessage("Hello World from doccy!");
-  });
+        if (filename === "cpp") {
+          let reg = /#include\s*[<"]([^>"]+)[>"]/g;
+          let match;
+          while ((match = reg.exec(documentContent)) !== null) {
+            console.log(match[1]);
+          }
+        } else if (filename == "py") {
+          let reg = /(?:import|from)\s([\w.]+)/g;
+          let match;
+          while ((match = reg.exec(documentContent)) !== null) {
+            console.log(match[1]);
+          }
+        } else if (filename == "java") {
+          let reg = /(?:import)\s([\w.]+)/g;
+          let match;
+          while ((match = reg.exec(documentContent)) !== null) {
+            console.log(match[1]);
+          }
+        }
+      }
+
+      // @ts-ignore
+      fetch(
+        "https://www.googleapis.com/customsearch/v1?key=AIzaSyC6gKMMoXgPkOnnC1TTj6C60aeLxI1v_ys&cx=32c6f4cdb22f343b0&q=tensorflow"
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          vscode.window.showInformationMessage(
+            `Data received: ${JSON.stringify(data)}`
+          );
+          console.log(JSON.stringify(data, null, 2));
+        })
+        .catch((error) => {
+          vscode.window.showErrorMessage(`Error: ${error}`);
+        });
+
+      let panel = vscode.window.createWebviewPanel(
+        "browser", // Identifies the type of the webview. Used internally
+        "Browser", // Title of the panel displayed to the user
+        vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+        {
+          enableScripts: true,
+        }
+      );
+
+      // Navigate to a website
+      panel.webview.html = `<html><body>Loading...</body></html>`;
+
+      // Use XMLHttpRequest to fetch the HTML content of the website
+      var XMLHttpRequest = require("xhr2");
+      let xhr = new XMLHttpRequest();
+      xhr.open("GET", "https://www.tensorflow.org", true);
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+          // Update the webview's HTML content
+          panel.webview.html = xhr.responseText;
+        }
+      };
+      xhr.send();
+
+      // Handle navigation events
+      panel.webview.onDidReceiveMessage(
+        (message) => {
+          switch (message.command) {
+            case "navigate":
+              xhr.open("GET", message.url, true);
+              xhr.send();
+              break;
+          }
+        },
+        null,
+        context.subscriptions
+      );
+
+      // Display a message box to the user
+      // vscode.window.showInformationMessage("Hello World from doccy!");
+    }
+  );
 
   context.subscriptions.push(disposable);
 }
