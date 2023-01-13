5cc2942597aeba4381bc96270679bb522a5a465d
Archit Jain <arcyjain2002@gmail.com>
2023-01-13 12:01:05 +0530

    initial commit

---
+++ doccy/extension.js
@@ -0,0 +1,72 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require("vscode");
+var path = require("path");
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "doccy" is now active!');
+
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand("getFileText", function () {
+    // The code you place here will be executed every time your command is executed
+    const editor = vscode.window.activeTextEditor;
+
+    // let document, filename;
+    if (editor) {
+      let document = editor.document;
+      let filename = path.basename(document.fileName);
+      // console.log(filename);
+      const documentContent = document.getText();
+      // console.log(documentContent);
+      for (var i = filename.length; i > 0; i--) {
+        if (filename[i] === ".") {
+          filename = filename.slice(i + 1, filename.length);
+          break;
+        }
+      }
+      vscode.window.showInformationMessage(filename);
+
+      if (filename === "cpp") {
+        let reg = /#include\s*[<"]([^>"]+)[>"]/g;
+        let match;
+        while ((match = reg.exec(documentContent)) !== null) {
+          console.log(match[1]);
+        }
+      } else if (filename == "py") {
+        let reg = /(?:import|from)\s([\w.]+)/g;
+        let match;
+        while ((match = reg.exec(documentContent)) !== null) {
+          console.log(match[1]);
+        }
+      } else if (filename == "java") {
+        let reg = /(?:import)\s([\w.]+)/g;
+        let match;
+        while ((match = reg.exec(documentContent)) !== null) {
+          console.log(match[1]);
+        }
+      }
+    }
+
+    // Display a message box to the user
+    // vscode.window.showInformationMessage("Hello World from doccy!");
+  });
+
+  context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+  activate,
+  deactivate,
+};
